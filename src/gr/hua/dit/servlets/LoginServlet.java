4a13a1ad609bf8efe7970ed05b46d1bfdea7327f
anargyros_tsadimas <tsadimas@gmail.com>
2016-08-22 14:16:51 +0300

    changed static user, password with hashtable of multiple users

--- src/gr/hua/dit/servlets/LoginServlet.java
+++ src/gr/hua/dit/servlets/LoginServlet.java
@@ -2,6 +2,8 @@ package gr.hua.dit.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Hashtable;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,8 +18,14 @@ import javax.servlet.http.HttpSession;
 public class LoginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 8345605719292191551L;
-	private final String userID = "admin";
-	private final String password = "password";
+	private static final Map<String, String> userDatabase = new Hashtable<>();
+
+	static {
+		userDatabase.put("user1", "user1pass");
+		userDatabase.put("user2", "user2pass");
+		userDatabase.put("user3", "user3pass");
+		userDatabase.put("user4", "user4pass");
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -26,11 +34,11 @@ public class LoginServlet extends HttpServlet {
 		String user = request.getParameter("user");
 		String pwd = request.getParameter("pwd");
 
-		if (userID.equals(user) && password.equals(pwd)) {
+		if (LoginServlet.userDatabase.containsKey(user) && (pwd.equals(LoginServlet.userDatabase.get(user)))) {
 			// if username and password match, create a new session
 			HttpSession session = request.getSession();
-			session.setAttribute("user", "Administrator");
-			// setting session to expiry in 30nai  mins
+			session.setAttribute("user", user);
+			// setting session to expiry in 30nai mins
 			session.setMaxInactiveInterval(30 * 60);
 			Cookie userName = new Cookie("user", user);
 			userName.setMaxAge(30 * 60);
